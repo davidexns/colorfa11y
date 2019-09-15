3f1d34f5551ad0b13bfcdb42b20351bd56203aac
David Evans <26511226+dev-evans@users.noreply.github.com>
2019-09-14 18:04:29 -0700

    fix cypress run command and remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-react-svg`,
       options: {
