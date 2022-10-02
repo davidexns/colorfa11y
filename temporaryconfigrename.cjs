30e315daa14189e02302bc00deee825871b2b582
davidexns <26511226+davidexns@users.noreply.github.com>
2022-10-01 18:57:55 -0700

    temp config rename to test plugin runs

---
+++ temporaryconfigrename.cjs
@@ -0,0 +1,11 @@
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+// 	e2e: {
+// 		baseUrl: 'http://localhost:1234',
+// 	},
+// })
+module.exports = {
+	baseUrl: 'http://localhost:3000',
+	video: false,
+}
