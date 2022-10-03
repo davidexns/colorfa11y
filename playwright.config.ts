ea9bb6d3155af911d4b714edc784e6f2ea4061c4
davidexns <26511226+davidexns@users.noreply.github.com>
2022-10-02 18:17:37 -0700

    try different config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ const config: PlaywrightTestConfig = {
 					port: 4173,
 				},
 		  }
-		: {}),
+		: { baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL }),
 	testDir: './tests',
 	/* Maximum time one test can run for. */
 	timeout: 30 * 1000,
